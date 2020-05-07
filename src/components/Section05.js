b81a65c5031f807c5536eeb2972fb591fe517fea
MohammadReza <md.mohammadreza+git@gmail.com>
2020-05-07 17:47:18 +0430

    initial Section05.js

---
+++ src/components/Section05.js
@@ -0,0 +1,189 @@
+import React from 'react';
+
+class Section05 extends React.Component {
+    render() {
+        return (
+            <div classname="row">
+                <div id="section05" className="container-fluid d-flex align-items-center bg-dark vh-100">
+                    <div className="container">
+                        <div className="card-columns text-center only-md">
+                            <div className="card p-3">
+                                <img src="public/../assets/img/karan-karnik-w7WVYjY8c5w-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/w7WVYjY8c5w">Photo by Karan Karnik on Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/eugenia-clara-95GPGCq_cdg-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/95GPGCq_cdg">Photo by Eugenia Clara on Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/irina-iriser-dxk6tMxNkJY-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/dxk6tMxNkJY">Photo by Irina Iriser on Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/nik-ramzi-nik-hassan-mbpKRQEjZis-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/mbpKRQEjZis">Photo by Nik Ramzi Nik Hassan on
+                                        Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/m.m.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <p className="rounded-pill bg-dark text-light">M.M</p>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/palash-jain-GxWZCbUolf0-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/GxWZCbUolf0">Photo by Palash Jain on Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/tony-reid-luanaxyvbpQ-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/luanaxyvbpQ">Photo by Tony Reid on Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3">
+                                <img src="public/../assets/img/thomas-millot-zndOvUadrTg-unsplash.jpg" alt className="card-img-top" />
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <a href="https://unsplash.com/photos/zndOvUadrTg">Photo by Thomas Millot on Unsplash</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="only-lg" style={{ display: 'none' }}>
+                            <div className="carousel slide" id="card-Sec5" data-ride="carousel" data-interval={10000}>
+                                <ul className="carousel-indicators">
+                                    <li data-target="#card-Sec5" data-slide-to={0} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={1} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={2} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={3} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={4} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={5} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={6} className="bg-light" />
+                                    <li data-target="#card-Sec5" data-slide-to={7} className="bg-light" />
+                                </ul>
+                                <div className="carousel-inner text-center">
+                                    <div className="carousel-item active">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/karan-karnik-w7WVYjY8c5w-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/w7WVYjY8c5w">Photo by Karan Karnik on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/eugenia-clara-95GPGCq_cdg-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/95GPGCq_cdg">Photo by Eugenia Clara on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/irina-iriser-dxk6tMxNkJY-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/dxk6tMxNkJY">Photo by Irina Iriser on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/nik-ramzi-nik-hassan-mbpKRQEjZis-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/mbpKRQEjZis">Photo by Nik Ramzi Nik
+                                                    Hassan on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/m.m.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <p className="rounded-pill bg-dark text-light">M.M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/palash-jain-GxWZCbUolf0-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/GxWZCbUolf0">Photo by Palash Jain on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/tony-reid-luanaxyvbpQ-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/luanaxyvbpQ">Photo by Tony Reid on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="card p-3">
+                                            <img src="public/../assets/img/thomas-millot-zndOvUadrTg-unsplash.jpg" alt className="card-img-top" />
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <a href="https://unsplash.com/photos/zndOvUadrTg">Photo by Thomas Millot on
+                                                    Unsplash</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#card-Sec5" className="carousel-control-prev" role="button" data-slide="prev">
+                                    <img className="only-md" src="public/../assets/img/jellyfish.png" />
+                                </a>
+                                <a href="#card-Sec5" className="carousel-control-next" role="button" data-slide="next">
+                                    <img className="only-md" src="public/../assets/img/jellyfish.png" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Section05;
