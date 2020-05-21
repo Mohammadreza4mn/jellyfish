3596e78fc323556eb3a577cef3507d2e27332558
MohammadReza <md.mohammadreza+git@gmail.com>
2020-05-21 13:34:38 +0430

    Updates

---
+++ precache-manifest.3cae2e97d8e8d3f2af742194e2206cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb85bcade61071c60d625d6edef65f2",
+    "url": "/jellyfish/index.html"
+  },
+  {
+    "revision": "440c992a3ca241e39187",
+    "url": "/jellyfish/static/css/main.71b2c3fd.chunk.css"
+  },
+  {
+    "revision": "a2ca6a56cb25578e4eba",
+    "url": "/jellyfish/static/js/2.678c6cbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jellyfish/static/js/2.678c6cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440c992a3ca241e39187",
+    "url": "/jellyfish/static/js/main.bed153fb.chunk.js"
+  },
+  {
+    "revision": "256d5416d9cd0612071e",
+    "url": "/jellyfish/static/js/runtime-main.56986f52.js"
+  }
+]);
